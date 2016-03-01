dd974db2625ae4dc102cdd00ad990ca0146d8036
Tom Smith <tomdcsmith@gmail.com>
2016-03-01 13:33:03 +0000

    Moved most package files out of nesting to top level.

---
+++ bin/check_for_local_schema.py
@@ -0,0 +1,3 @@
+import eva_cttv_pipeline.utilities as utilities
+
+utilities.check_for_local_schema()
--- eva_cttv_pipeline/bin/check_for_local_schema.py
+++
@@ -1,3 +0,0 @@
-import eva_cttv_pipeline.utilities as utilities
-
-utilities.check_for_local_schema()
