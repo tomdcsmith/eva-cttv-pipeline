35ed98bd765420f1699f6f25847d03a7294b84fa
Tom Smith <tomdcsmith@gmail.com>
2016-04-05 15:35:00 +0100

    Config file for tests. More tests for utilities.py.

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,7 +1,79 @@
 import os
 import unittest
+import shutil
 
 import eva_cttv_pipeline.utilities as util
+
+
+class GetResourceFileTest(unittest.TestCase):
+    def test_get_resource_file_existent(self):
+        self.assertTrue(util.get_resource_file("eva_cttv_pipeline", "resources/json_schema"))
+
+    def test_get_resource_file_nonexistent(self):
+        self.assertEqual(util.get_resource_file("not_a_real_package_39146", "not_a_real_file"), None)
+
+
+class CopyAndOverwriteTest(unittest.TestCase):
+    def setUp(self):
+        self.test_dir_a = os.path.join(os.path.dirname(__file__), "resources", "test_tmp_a")
+        self.test_dir_b = os.path.join(os.path.dirname(__file__), "resources", "test_tmp_b")
+        # self.test_dir_c = os.path.join(os.path.dirname(__file__), "resources", "test_tmp_c")
+        os.makedirs(self.test_dir_a)
+        os.makedirs(self.test_dir_b)
+        self.test_file_a = os.path.join(self.test_dir_a, "test.txt")
+        self.test_string = "this is a test string"
+        with open(self.test_file_a, "wt") as f:
+            f.write(self.test_string)
+
+    def tearDown(self):
+        shutil.rmtree(self.test_dir_a)
+        shutil.rmtree(self.test_dir_b)
+        # shutil.rmtree(self.test_dir_c)
+
+    def test_existing(self):
+        test_file_b = os.path.join(self.test_dir_b, "test.txt")
+        with open(test_file_b, "wt") as f:
+            f.write("hello world")
+        util.copy_and_overwrite(self.test_dir_a, self.test_dir_b)
+        with open(test_file_b, "rt") as f:
+            contents = f.read()
+        self.assertEqual(contents, self.test_string)
+
+
+class CopyDirTest(unittest.TestCase):
+    def setUp(self):
+        self.test_dir_a = os.path.join(os.path.dirname(__file__), "resources", "test_tmp_a")
+        self.test_dir_b = os.path.join(os.path.dirname(__file__), "resources", "test_tmp_b")
+        self.test_dir_c = os.path.join(os.path.dirname(__file__), "resources", "test_tmp_c")
+        os.makedirs(self.test_dir_a)
+        os.makedirs(self.test_dir_b)
+        self.test_file_a = os.path.join(self.test_dir_a, "test.txt")
+        self.test_string = "this is a test string"
+        with open(self.test_file_a, "wt") as f:
+            f.write(self.test_string)
+
+    def tearDown(self):
+        shutil.rmtree(self.test_dir_a)
+        shutil.rmtree(self.test_dir_b)
+
+    def test_existing(self):
+        test_file_b = os.path.join(self.test_dir_b, "test.txt")
+        with open(test_file_b, "wt") as f:
+            f.write("hello world")
+        util.copy_dir(self.test_dir_a, self.test_dir_b)
+        with open(test_file_b, "rt") as f:
+            contents = f.read()
+        self.assertEqual(contents, self.test_string)
+
+    def test_nonexisting(self):
+        util.copy_dir(self.test_dir_a, self.test_dir_c)
+        with open(os.path.join(self.test_dir_c, "test.txt"), "rt") as f:
+            contents = f.read()
+        self.assertEqual(contents, self.test_string)
+        shutil.rmtree(self.test_dir_c)
+
+
+# TODO test change_json_refs, create_local_schema, check_for_local_schema
 
 
 class ArgParserTest(unittest.TestCase):
@@ -37,19 +109,9 @@ class ArgParserTest(unittest.TestCase):
         self.assertEquals(self.argparser.variant_summary_file, self.variant_summary_file)
 
 
-class GetResourceFileTest(unittest.TestCase):
-    def test_get_resource_file_existent(self):
-        self.assertTrue(util.get_resource_file("eva_cttv_pipeline", "resources/json_schema"))
-
-    def test_get_resource_file_nonexistent(self):
-        self.assertEqual(util.get_resource_file("not_a_real_package_39146", "not_a_real_file"), None)
-
-
 class CheckDirExistsCreateTest(unittest.TestCase):
     def test_create(self):
         directory = "./test_tmp"
         util.check_dir_exists_create(directory)
         self.assertTrue(os.path.exists(directory))
-        os.removedirs(directory)
-
-# TODO test other filesystem-related functions
+        os.rmdir(directory)
