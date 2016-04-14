34325ed508db015564d50109d90967fb0d2accc0
Tom Smith <tomdcsmith@gmail.com>
2016-04-14 14:12:27 +0100

    Finished efoterm tests.

--- tests/test_efo_term.py
+++ tests/test_efo_term.py
@@ -7,18 +7,18 @@ import eva_cttv_pipeline.efo_term as EFOT
 
 
 class EFOTermTest(unittest.TestCase):
-    # def setUpClass(cls):
-    #     cls.obsolete_terms = ["ob_term_1", "ob_term_2"]
-    #     cls.cttv_available_terms = ["cttv_av_term_1", "cttv_av_term_2"]
+    @classmethod
+    def setUpClass(cls):
+        EFOT.EFOTerm.obsolete_terms = {"ob_id_1": "ob_term_1", "ob_id_2": "ob_term_2"}
+        EFOT.EFOTerm.cttv_available_terms = {"cttv_id_1": "cttv_av_term_1", "cttv_id_2": "cttv_av_term_2"}
 
-    def setUp(self):
-        self.test_efoterm_id = EFOT.EFOTerm("test_id")
+        cls.test_efoterm_id = EFOT.EFOTerm("test_id")
 
-        self.test_efoterm_ob_a = EFOT.EFOTerm("ob_term_1")
-        self.test_efoterm_ob_b = EFOT.EFOTerm("not_ob")
+        cls.test_efoterm_ob_a = EFOT.EFOTerm("ob_id_1")
+        cls.test_efoterm_ob_b = EFOT.EFOTerm("not_ob")
 
-        self.test_efoterm_cttv_a = EFOT.EFOTerm("cttv_av_term_1")
-        self.test_efoterm_cttv_b = EFOT.EFOTerm("not_cttv_av")
+        cls.test_efoterm_cttv_a = EFOT.EFOTerm("cttv_id_1")
+        cls.test_efoterm_cttv_b = EFOT.EFOTerm("not_cttv_av")
 
     def test_efoid(self):
         self.assertEqual(self.test_efoterm_id.efoid, "test_id")
@@ -26,5 +26,10 @@ class EFOTermTest(unittest.TestCase):
         self.assertNotEquals(self.test_efoterm_id.efoid, "test_id")
         self.assertEqual(self.test_efoterm_id.efoid, "new_id")
 
-    # def test_is_obsolete(self):
-    #     self.assertRaises(EFOT.EFOTerm.IsObsoleteException, self.test_efoterm_ob_a.is_obsolete())
+    def test_is_obsolete(self):
+        self.assertRaises(EFOT.EFOTerm.IsObsoleteException, self.test_efoterm_ob_a.is_obsolete)
+        self.assertFalse(self.test_efoterm_ob_b.is_obsolete())
+
+    def test_is_cttv_available(self):
+        self.assertRaises(EFOT.EFOTerm.NotCttvAvailableException, self.test_efoterm_cttv_b.is_cttv_available)
+        self.assertTrue(self.test_efoterm_cttv_a.is_cttv_available())
