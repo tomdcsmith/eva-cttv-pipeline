28311226da6cde15015b3b316b92528c87900fde
Tom Smith <tomdcsmith@gmail.com>
2016-04-15 10:51:57 +0100

    Removed unused "get_ranked_so_names"

--- tests/test_consequence_type.py
+++ tests/test_consequence_type.py
@@ -40,9 +40,6 @@ class SoTermTest(unittest.TestCase):
         self.assertEqual(self.test_so_term_a.rank, 3)
         self.assertEqual(self.test_so_term_b.rank, 34)
 
-    def test_get_ranked_so_names(self):
-        self.assertEqual(CT.SoTerm.get_ranked_so_names(), ['transcript_ablation', 'splice_acceptor_variant', 'splice_donor_variant', 'stop_gained', 'frameshift_variant', 'stop_lost', 'initiator_codon_variant', 'transcript_amplification', 'inframe_insertion', 'inframe_deletion', 'missense_variant', 'splice_region_variant', 'incomplete_terminal_codon_variant', 'stop_retained_variant', 'synonymous_variant', 'coding_sequence_variant', 'mature_miRNA_variant', '5_prime_UTR_variant', '3_prime_UTR_variant', 'non_coding_transcript_exon_variant', 'intron_variant', 'NMD_transcript_variant', 'non_coding_transcript_variant', 'upstream_gene_variant', 'downstream_gene_variant', 'TFBS_ablation', 'TFBS_amplification', 'TF_binding_site_variant', 'regulatory_region_ablation', 'regulatory_region_amplification', 'regulatory_region_variant', 'feature_elongation', 'feature_truncation', 'intergenic_variant'])
-
 
 class ConsequenceTypeTest(unittest.TestCase):
     def setUp(self):
