b12d7049bcc85a790057cf43c8182f28a07e7d8b
Tom Smith <tomdcsmith@gmail.com>
2016-04-18 11:31:03 +0100

    -Added __repr__ function to EFOTerm -Moved clin_sig_2_activity to evidence_strings.py, rather than passing as argument -Removed get evidence string tests in test_clinvar_to_evidence_strigns.py -Tests for ev string constructors in test_evidence_strings.py -New "clear" methods in ev string classes for use in testing

--- eva_cttv_pipeline/evidence_strings.py
+++ eva_cttv_pipeline/evidence_strings.py
@@ -15,6 +15,9 @@ GEN_SCHEMA_FILE = utilities.get_resource_file(__package__, config.local_schema +
 SOM_SCHEMA_FILE = utilities.get_resource_file(__package__, config.local_schema + "/src/literature_curated.json")
 
 
+clin_sig_2_activity = {'other': 'http://identifiers.org/cttv.activity/unknown', 'unknown': 'http://identifiers.org/cttv.activity/unknown', 'protective': 'http://identifiers.org/cttv.activity/tolerated_by_target', 'probable-pathogenic': 'http://identifiers.org/cttv.activity/predicted_damaging', 'non-pathogenic': 'http://identifiers.org/cttv.activity/tolerated_by_target', 'benign': 'http://identifiers.org/cttv.activity/tolerated_by_target', 'likely pathogenic': 'http://identifiers.org/cttv.activity/predicted_damaging', 'probable-non-pathogenic': 'http://identifiers.org/cttv.activity/predicted_tolerated', 'pathogenic': 'http://identifiers.org/cttv.activity/damaging_to_target', 'association': 'http://identifiers.org/cttv.activity/damaging_to_target', 'conflicting data from submitters': 'http://identifiers.org/cttv.activity/unknown', 'uncertain significance': 'http://identifiers.org/cttv.activity/unknown', 'likely benign': 'http://identifiers.org/cttv.activity/predicted_tolerated', 'histocompatibility': 'http://identifiers.org/cttv.activity/unknown', 'not provided': 'http://identifiers.org/cttv.activity/unknown', 'untested': 'http://identifiers.org/cttv.activity/unknown', 'confers sensitivity': 'http://identifiers.org/cttv.activity/predicted_damaging', 'drug-response': 'http://identifiers.org/cttv.activity/unknown', 'risk factor': 'http://identifiers.org/cttv.activity/predicted_damaging'}
+
+
 def get_cttv_variant_type(ref, alt):
     if len(ref) < 2 and len(alt) < 2:
         cttv_variant_type = 'snp single'
@@ -35,6 +38,9 @@ class CTTVEvidenceString(UserDict):
     def add_unique_association_field(self, key, value):
         self['unique_association_fields'][key] = value
 
+    def _clear_target(self):
+        self['target']['id'] = []
+
     def set_target(self, id, activity):
         self['target']['id'].append(id)
         self['target']['activity'] = activity
@@ -44,8 +50,8 @@ class CTTVEvidenceString(UserDict):
         return efo_term.EFOTerm(self['disease']['id'][0])
 
     @disease.setter
-    def disease(self, id):
-        self['disease']['id'].append(id)
+    def disease(self, value):
+        self['disease']['id'] = [value]
 
     @property
     def evidence_codes(self):
@@ -67,11 +73,9 @@ class CTTVEvidenceString(UserDict):
 class CTTVGeneticsEvidenceString(CTTVEvidenceString):
     schema = json.loads(open(GEN_SCHEMA_FILE, 'r').read())
 
-    def __init__(self, efo_list, clin_sig, clin_sig_2_activity, clinvarRecord,
-                                      consequenceType, ensembl_gene_id, ensembl_gene_id_uri, ensembl_gene_id_uris,
-                                      measure_set_refs_list, n_more_than_one_efo_term, observed_refs_list, rcv_to_gene_evidence_codes,
-                                      record, rs, trait_counter, traits_ref_list, traits,
-                                      unrecognised_clin_sigs):
+    def __init__(self, efo_list, clin_sig, clinvarRecord, consequenceType, ensembl_gene_id,
+                 ensembl_gene_id_uri, measure_set_refs_list, observed_refs_list, rcv_to_gene_evidence_codes, record, rs,
+                 trait_counter, traits_ref_list, unrecognised_clin_sigs):
 
         a_dictionary = {'sourceID': 'eva',
                                      'validated_against_schema_version': '1.2.2',
@@ -262,6 +266,9 @@ class CTTVGeneticsEvidenceString(CTTVEvidenceString):
         #     sys.exit()
         self.disease.is_obsolete()
 
+    def _clear_variant(self):
+        self['variant']['id'] = []
+
     def set_variant(self, id, type):
         self['variant']['id'].append(id)
         self['variant']['type'] = type
@@ -290,9 +297,9 @@ class CTTVGeneticsEvidenceString(CTTVEvidenceString):
 class CTTVSomaticEvidenceString(CTTVEvidenceString):
     schema = json.loads(open(SOM_SCHEMA_FILE, 'r').read())
 
-    def __init__(self, efo_list, clin_sig, clin_sig_2_activity, clinvarRecord,
-                                     ensembl_gene_id, ensembl_gene_id_uri, ensembl_gene_id_uris, measure_set_refs_list,
-                                     n_more_than_one_efo_term, observed_refs_list, trait_counter, trait_refs_list, traits,
+    def __init__(self, efo_list, clin_sig, clinvarRecord,
+                                     ensembl_gene_id, ensembl_gene_id_uri, measure_set_refs_list,
+                                     observed_refs_list, trait_counter, trait_refs_list,
                                      unrecognised_clin_sigs, consequenceType):
         a_dictionary = {'sourceID': 'eva_somatic',
                                      'validated_against_schema_version': '1.2.2',
@@ -417,6 +424,9 @@ class CTTVSomaticEvidenceString(CTTVEvidenceString):
     @date.setter
     def date(self, date_string):
         self['evidence']['date_asserted'] = date_string
+
+    def _clear_known_mutations(self):
+        self['evidence']['known_mutations'] = []
 
     def add_known_mutation(self, new_functional_consequence, so_name):
         new_known_mutation = {'functional_consequence': new_functional_consequence, 'preferred_name': so_name}
