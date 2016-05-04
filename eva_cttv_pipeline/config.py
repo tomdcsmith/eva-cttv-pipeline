f01f0fe4d927d89914f27470273182855e4f306b
Tom Smith <tomdcsmith@gmail.com>
2016-05-04 14:41:23 +0100

    Moved cellbase and output configuration to config.py

--- eva_cttv_pipeline/config.py
+++ eva_cttv_pipeline/config.py
@@ -1,5 +1,21 @@
 local_schema = "resources/schema_local"
 
+#########################################
+# clinvar_to_evidence_strings.py settings
+#########################################
+# cellbase
+BATCH_SIZE = 200
+HOST = 'www.ebi.ac.uk'
+
+# output settings
+EVIDENCESTRINGSFILENAME = 'evidence_strings.json'
+EVIDENCERECORDSFILENAME = 'evidence_records.tsv'
+UNMAPPEDTRAITSFILENAME = 'unmappedTraits.tsv'
+UNAVAILABLEEFOFILENAME = 'unavailableefo.tsv'
+NSVLISTFILE = 'nsvlist.txt'
+TMPDIR = '/tmp/'
+
+
 sparqlep = ""
 sparql_user = ""
 sparql_password = ""
