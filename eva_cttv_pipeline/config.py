d51314a0b7d92431411adb3766eda690b7eb61d5
Tom Smith <tomdcsmith@gmail.com>
2016-05-04 16:18:10 +0100

    Moved evidence_strings settings to config file. Moved references to config variables from top of files, to where they're used.

--- eva_cttv_pipeline/config.py
+++ eva_cttv_pipeline/config.py
@@ -1,5 +1,11 @@
 local_schema = "resources/schema_local"
 
+
+sparqlep = ""
+sparql_user = ""
+sparql_password = ""
+
+
 #########################################
 # clinvar_to_evidence_strings.py settings
 #########################################
@@ -13,9 +19,16 @@ EVIDENCERECORDSFILENAME = 'evidence_records.tsv'
 UNMAPPEDTRAITSFILENAME = 'unmappedTraits.tsv'
 UNAVAILABLEEFOFILENAME = 'unavailableefo.tsv'
 NSVLISTFILE = 'nsvlist.txt'
-TMPDIR = '/tmp/'
+
+######
 
 
-sparqlep = ""
-sparql_user = ""
-sparql_password = ""
+##############################
+# evidence_strings.py settings
+##############################
+GEN_EV_STRING_JSON = "resources/CTTVGeneticsEvidenceString.json"
+SOM_EV_STRING_JSON = "resources/CTTVSomaticEvidenceString.json"
+GEN_SCHEMA_FILE = local_schema + "/src/genetics.json"
+SOM_SCHEMA_FILE = local_schema + "/src/literature_curated.json"
+
+#############
