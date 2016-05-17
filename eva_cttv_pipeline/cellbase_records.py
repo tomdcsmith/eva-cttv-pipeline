a4e286007520ae4513c5ed7f8bc478467f531c1e
Tom Smith <tomdcsmith@gmail.com>
2016-05-17 12:21:27 +0100

    Cellbase records into class (generator)

--- eva_cttv_pipeline/cellbase_records.py
+++ eva_cttv_pipeline/cellbase_records.py
@@ -5,30 +5,31 @@ import urllib.request
 from eva_cttv_pipeline import config
 
 
-def get_curr_response(skip, limit=config.BATCH_SIZE):
-    reader = codecs.getreader("utf-8")
-    answer = urllib.request.urlopen('http://' + config.HOST + '/cellbase/webservices/rest/v3/hsapiens/feature/clinical/all?source=clinvar&skip=' + str(skip) + '&limit=' + str(limit))
-    curr_response = json.load(reader(answer))['response'][0]
-    return curr_response
+class CellbaseRecords:
+    def __init__(self, limit=config.BATCH_SIZE):
+        self.limit = limit
 
+    def __iter__(self):
+        for curr_result_list in self.__get_curr_result_lists():
+            for record in curr_result_list:
+                yield record
 
-def get_curr_result_list(curr_response):
-    curr_result_list = curr_response['result']
-    return curr_result_list
+    def __get_curr_response(self, skip):
+        reader = codecs.getreader("utf-8")
+        answer = urllib.request.urlopen('http://' + config.HOST + '/cellbase/webservices/rest/v3/hsapiens/feature/clinical/all?source=clinvar&skip=' + str(skip) + '&limit=' + str(self.limit))
+        curr_response = json.load(reader(answer))['response'][0]
+        return curr_response
 
+    def __get_curr_result_list(self, curr_response):
+        curr_result_list = curr_response['result']
+        return curr_result_list
 
-def get_curr_result_lists():
-    skip = 0
-    while True:
-        curr_response = get_curr_response(skip)
-        curr_result_list = get_curr_result_list(curr_response)
-        if len(curr_result_list) == 0:
-            break
-        skip += config.BATCH_SIZE
-        yield curr_result_list
-
-
-def get_records():
-    for curr_result_list in get_curr_result_lists():
-        for record in curr_result_list:
-            yield record
+    def __get_curr_result_lists(self):
+        skip = 0
+        while True:
+            curr_response = self.__get_curr_response(skip)
+            curr_result_list = self.__get_curr_result_list(curr_response)
+            if len(curr_result_list) == 0:
+                break
+            skip += config.BATCH_SIZE
+            yield curr_result_list
--- tests/test_cellbase_records.py
+++ tests/test_cellbase_records.py
@@ -4,24 +4,28 @@ from eva_cttv_pipeline import cellbase_records, config
 
 
 class CellbaseRecordsTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.cb_records = cellbase_records.CellbaseRecords()
+
     def test_get_curr_result_list(self):
-        curr_response = cellbase_records.get_curr_response(0)
-        curr_result_list = cellbase_records.get_curr_result_list(curr_response)
+        curr_response = self.cb_records._CellbaseRecords__get_curr_response(0)
+        curr_result_list = self.cb_records._CellbaseRecords__get_curr_result_list(curr_response)
         self.assertEqual(len(curr_result_list), config.BATCH_SIZE)
 
-        curr_response = cellbase_records.get_curr_response(0)
-        curr_response_test = cellbase_records.get_curr_response(curr_response['numTotalResults'])
-        curr_result_list_test = cellbase_records.get_curr_result_list(curr_response_test)
+        curr_response = self.cb_records._CellbaseRecords__get_curr_response(0)
+        curr_response_test = self.cb_records._CellbaseRecords__get_curr_response(curr_response['numTotalResults'])
+        curr_result_list_test = self.cb_records._CellbaseRecords__get_curr_result_list(curr_response_test)
         self.assertEqual(len(curr_result_list_test), 0)
 
-        curr_response = cellbase_records.get_curr_response(999999)
-        curr_result_list = cellbase_records.get_curr_result_list(curr_response)
+        curr_response = self.cb_records._CellbaseRecords__get_curr_response(999999)
+        curr_result_list = self.cb_records._CellbaseRecords__get_curr_result_list(curr_response)
         self.assertEqual(len(curr_result_list), 0)
 
-        curr_response = cellbase_records.get_curr_response(0)
+        curr_response = self.cb_records._CellbaseRecords__get_curr_response(0)
         len_to_expect = 20
-        curr_response = cellbase_records.get_curr_response(curr_response['numTotalResults'] - len_to_expect)
-        curr_result_list = cellbase_records.get_curr_result_list(curr_response)
+        curr_response = self.cb_records._CellbaseRecords__get_curr_response(curr_response['numTotalResults'] - len_to_expect)
+        curr_result_list = self.cb_records._CellbaseRecords__get_curr_result_list(curr_response)
         self.assertEqual(len(curr_result_list), len_to_expect)
 
     # def test_get_curr_result_lists(self):
