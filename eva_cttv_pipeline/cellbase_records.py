6e318c250d035e2b50e15f4cb1bfddd2bebef20a
Tom Smith <tomdcsmith@gmail.com>
2016-05-23 12:08:09 +0100

    Made skip an attribute. Removed extra __get_curr_result_lists method.

--- eva_cttv_pipeline/cellbase_records.py
+++ eva_cttv_pipeline/cellbase_records.py
@@ -7,12 +7,17 @@ from eva_cttv_pipeline import config
 
 class CellbaseRecords:
     def __init__(self, limit=config.BATCH_SIZE):
+        self.skip = 0
         self.limit = limit
 
     def __iter__(self):
-        for curr_result_list in self.__get_curr_result_lists():
+        while True:
+            curr_result_list = self.__get_curr_result_list()
+            if not curr_result_list:
+                break
             for record in curr_result_list:
                 yield record
+            self.skip += config.BATCH_SIZE
 
     def __get_curr_response(self, skip):
         reader = codecs.getreader("utf-8")
@@ -20,16 +25,9 @@ class CellbaseRecords:
         curr_response = json.load(reader(answer))['response'][0]
         return curr_response
 
-    def __get_curr_result_list(self, curr_response):
+    def __get_curr_result_list(self):
+        curr_response = self.__get_curr_response(self.skip)
         curr_result_list = curr_response['result']
+        if len(curr_result_list) == 0:
+            return None
         return curr_result_list
-
-    def __get_curr_result_lists(self):
-        skip = 0
-        while True:
-            curr_response = self.__get_curr_response(skip)
-            curr_result_list = self.__get_curr_result_list(curr_response)
-            if len(curr_result_list) == 0:
-                break
-            skip += config.BATCH_SIZE
-            yield curr_result_list
