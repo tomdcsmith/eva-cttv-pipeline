c1ea17be2344ee1c276e1bf8583b665b134fac55
Tom Smith <tomdcsmith@gmail.com>
2016-06-14 16:00:00 +0100

    Added multiple attempts for cellbase request

--- eva_cttv_pipeline/cellbase_records.py
+++ eva_cttv_pipeline/cellbase_records.py
@@ -1,5 +1,7 @@
 import codecs
 import json
+import sys
+import urllib.error
 import urllib.request
 
 from eva_cttv_pipeline import config
@@ -26,9 +28,21 @@ class CellbaseRecords:
         reader = codecs.getreader("utf-8")
         url = 'http://{}/cellbase/webservices/rest/v3/hsapiens/feature/clinical/' \
               'all?source=clinvar&skip={}&limit={}'.format(config.HOST, self.skip, self.limit)
-        answer = urllib.request.urlopen(url)
-        curr_response = json.load(reader(answer))['response'][0]
-        return curr_response
+
+        attempts = 0
+        curr_response = None
+        while attempts < 5:
+            try:
+                answer = urllib.request.urlopen(url)
+                curr_response = json.load(reader(answer))['response'][0]
+            except urllib.error.HTTPError as e:
+                attempts += 1
+
+        if curr_response:
+            return curr_response
+        else:
+            print("Response from url {} failed".format(url))
+            sys.exit(1)
 
     def __get_curr_result_list(self):
         curr_response = self.__get_curr_response()
