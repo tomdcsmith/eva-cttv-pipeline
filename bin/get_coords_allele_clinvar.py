37ac58d600a87b895343828fa6bccaa9a60686d0
Tom Smith <tomdcsmith@gmail.com>
2016-06-21 12:00:52 +0100

    - New argument for specifying to output structural variants - New skip line logic for SVs - New function to create lines for SV info

--- bin/get_coords_allele_clinvar.py
+++ bin/get_coords_allele_clinvar.py
@@ -4,7 +4,7 @@ import subprocess
 import sys
 
 
-COUNTER_BAD_RS_OR_AL = 0
+COUNTER = 0
 
 
 class ArgParser:
@@ -13,31 +13,38 @@ class ArgParser:
 
         parser.add_argument("-i", dest="infilepath", required=True)
         parser.add_argument("-o", dest="outfilepath", required=True)
+        parser.add_argument('-s', dest="structural", action='store_true')
 
         args = parser.parse_args(args=argv[1:])
 
         self.infilepath = args.infilepath
         self.outfilepath = args.outfilepath
+        self.structural = args.structural
 
 
-def skip_line(record):
-    global COUNTER_BAD_RS_OR_AL
+def skip_line(record, structural):
+    global COUNTER
     assembly = record[12]
     if assembly != "GRCh38":
         return True
     clin_sig = record[5]
     if all(x not in clin_sig for x in ["Pathogenic", "Likely Pathogenic"]):
         return True
-    rsid = record[6]
-    ref = record[25]
-    alt = record[26]
-    if not ((ref != "na" and alt != "na") or rsid != "-1"):
-        COUNTER_BAD_RS_OR_AL += 1
-        return True
+    if structural:
+        nsv = record[7]
+        if nsv == "-":
+            return True
+        COUNTER += 1
+    else:
+        rsid = record[6]
+        ref = record[25]
+        alt = record[26]
+        if not ((ref != "na" and alt != "na") or rsid != "-1"):
+            return True
     return False
 
 
-def make_output_line(record):
+def make_output_lines(record):
     chrom = record[13]
     start = record[14]
     end = record[15]
@@ -48,25 +55,51 @@ def make_output_line(record):
     else:
         var_id = "{}_{}_{}_{}_{}".format(chrom, start, end, ref, alt)
     allele = "{}/{}".format(ref, alt)
+    output_lines = []
     output_line = '\t'.join([chrom, start, end, allele, "+", var_id, "\n"])
-    return output_line
+    output_lines.append(output_line)
+    return output_lines
 
 
-def process_line(line, outfile):
+def make_output_lines_struct(record):
+    chrom = record[13]
+    start = record[14]
+    end = record[15]
+    ref = record[25]
+    alt = record[26]
+    rcvs = record[8].split(";")
+    nsv = record[7]
+    gene_id = record[3]
+    pheno_ids = record[10]
+
+    allele = "{}/{}".format(ref, alt)
+
+    output_lines = []
+    for rcv in rcvs:
+        output_line = '\t'.join([chrom, start, end, allele, "+", rcv, nsv, gene_id, pheno_ids]) + "\n"
+        output_lines.append(output_line)
+    return output_lines
+
+
+def process_line(line, outfile, structural):
     line = line.rstrip()
     record = line.split("\t")
-    if skip_line(record):
+    if skip_line(record, structural):
         return
-    output_line = make_output_line(record)
-    outfile.write(output_line)
+    if structural:
+        output_lines = make_output_lines_struct(record)
+    else:
+        output_lines = make_output_lines(record)
+    for output_line in output_lines:
+        outfile.write(output_line)
 
 
-def process_file(infilepath, outfilepath):
+def process_file(infilepath, outfilepath, structural):
     with gzip.open(infilepath, "rt") as infile:
         with gzip.open(outfilepath, "wt") as outfile:
             infile.readline()
             for line in infile:
-                process_line(line, outfile)
+                process_line(line, outfile, structural)
 
 
 def post_process(outfilepath):
@@ -83,11 +116,11 @@ def main():
 
     args = ArgParser(sys.argv)
 
-    process_file(args.infilepath, args.outfilepath)
+    process_file(args.infilepath, args.outfilepath, args.structural)
 
     post_process(args.outfilepath)
 
-    print(COUNTER_BAD_RS_OR_AL)
+    print(COUNTER)
 
 
 if __name__ == '__main__':
