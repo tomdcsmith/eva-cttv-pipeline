89ba2dbaadaa0bf483a3da64ccf9aeccccc0f783
Tom Smith <tomdcsmith@gmail.com>
2016-07-19 17:11:51 +0100

    Allow cellbase records to accept json file as argument, and number of records to skip if using webservice

--- eva_cttv_pipeline/cellbase_records.py
+++ eva_cttv_pipeline/cellbase_records.py
@@ -1,32 +1,45 @@
 import codecs
 import json
 import urllib.request
+import urllib.error
 
-from eva_cttv_pipeline import config
+from eva_cttv_pipeline import config, utilities
 
 
 class CellbaseRecords:
 
     """Assists in the requesting and iteration of clinvar cellbase records"""
 
-    def __init__(self, limit=config.BATCH_SIZE):
-        self.skip = 0
+    def __init__(self, limit=config.BATCH_SIZE, skip=0, json_file=None):
+        self.skip = skip
         self.limit = limit
+        self.json_file = json_file
 
     def __iter__(self):
-        while True:
-            curr_result_list = self.__get_curr_result_list()
-            if not curr_result_list:
-                break
-            for record in curr_result_list:
+        if not self.json_file:
+            while True:
+                curr_result_list = self.__get_curr_result_list()
+                if not curr_result_list:
+                    break
+                for record in curr_result_list:
+                    yield record
+                self.skip += config.BATCH_SIZE
+        else:
+            for record in self.__each_line_in_file():
                 yield record
-            self.skip += config.BATCH_SIZE
 
     def __get_curr_response(self):
         reader = codecs.getreader("utf-8")
         url = 'http://{}/cellbase/webservices/rest/v3/hsapiens/feature/clinical/' \
               'all?source=clinvar&skip={}&limit={}'.format(config.HOST, self.skip, self.limit)
-        answer = urllib.request.urlopen(url)
+        for _ in range(3):
+            while True:
+                try:
+                    answer = urllib.request.urlopen(url)
+                except Exception:
+                    continue
+                raise urllib.error.HTTPError
+
         curr_response = json.load(reader(answer))['response'][0]
         return curr_response
 
@@ -36,3 +49,8 @@ class CellbaseRecords:
         if len(curr_result_list) == 0:
             return None
         return curr_result_list
+
+    def __each_line_in_file(self):
+        with utilities.open_file(self.json_file, "rt") as f:
+            for line in f:
+                yield line.rstrip()
