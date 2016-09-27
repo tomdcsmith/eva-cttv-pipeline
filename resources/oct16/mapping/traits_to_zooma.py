9fe183bdc0f4ef3ce6cb303c397644f9965a2fbd
Tom Smith <tsmith@ebi.ac.uk>
2016-09-27 13:28:40 +0100

    better handling of filters

--- resources/oct16/mapping/traits_to_zooma.py
+++ resources/oct16/mapping/traits_to_zooma.py
@@ -7,14 +7,16 @@ def main():
     parser = ArgParser(sys.argv)
 
     trait_name_tuples = get_trait_name_tuples_from_file(parser.input_filepath)
-    parse_and_write_output(trait_name_tuples, parser.output_filepath)
+    parse_and_write_output(trait_name_tuples, parser.output_filepath, parser.filters)
 
 
-def parse_and_write_output(trait_name_tuples, output_filepath):
+def parse_and_write_output(trait_name_tuples, output_filepath, filters):
     with open(output_filepath, "wt") as output_file:
         for trait_name_tuple in trait_name_tuples:
             trait_name = trait_name_tuple[0]
-            zooma_response = query_zooma(trait_name)
+            zooma_response = query_zooma(trait_name, filters)
+            if zooma_response == 0:
+                continue
             output_list = zooma_response_to_list(zooma_response, trait_name_tuple)
 
             output_file.write("\t".join(output_list) + "\n")
@@ -33,27 +35,36 @@ def get_trait_name_tuples_from_file(filepath):
 
 
 # def form_zooma_query(property_value, ontologies, required, preferred):
-def form_zooma_query(trait_name):
-    url = "http://snarf.ebi.ac.uk:8580/spot/zooma/v2/api/services/annotate?propertyValue={}&filter=ontologies:[efo,hp,ordo],required:[none]".format(trait_name)
+def form_zooma_query(trait_name, filters):
+    url_filters = []
+    url = "http://snarf.ebi.ac.uk:8580/spot/zooma/v2/api/services/annotate?propertyValue={}".format(trait_name)
+    url_filters.append("required:[{}]".format(filters["required"]))
+    url_filters.append("ontologies:[{}]".format(filters["ontologies"]))
+    url_filters.append("preferred:[{}]".format(filters["preferred"]))
+    url += "&filter={}".format(",".join(url_filters))
+
     return url
 
 
 # def query_zooma(property_value, ontologies=None, required=None, preferred=None):
-def query_zooma(trait_name):
-    url = form_zooma_query(trait_name)
+def query_zooma(trait_name, filters):
+    url = form_zooma_query(trait_name, filters)
     r = requests.get(url)
-    return r.json()
+    try:
+        json_response = r.json()
+    except Exception as e:
+        # print("\t".join([trait_name, e, r]))
+        return 0
+    return json_response
 
 
 def result_to_tuple(result):
     label = result["annotatedProperty"]["propertyValue"]
-    if len(result["semanticTags"]) > 1:
-        print(result)
-        sys.exit()
-    uri = result["semanticTags"][0]
+    uri = ",".join(result["semanticTags"])
     confidence = result["confidence"]
+    source_name = result["derivedFrom"]["provenance"]["source"]["name"]
 
-    result_tuple = (label, uri, confidence)
+    result_tuple = (label, uri, confidence, source_name)
 
     return result_tuple
 
@@ -73,13 +84,18 @@ class ArgParser:
     def __init__(self, argv):
         parser = argparse.ArgumentParser()
 
-        parser.add_argument("-i", dest="input_filepath")
-        parser.add_argument("-o", dest="output_filepath")
+        parser.add_argument("-i", dest="input_filepath", required=True)
+        parser.add_argument("-o", dest="output_filepath", required=True)
+        parser.add_argument("-n", dest="ontologies", default="efo,hp,ordo")
+        parser.add_argument("-r", dest="required", default="bmb-wp7,cttv,eva-clinvar,sysmicro,atlas,uniprot,gwas,ebisc")
+        parser.add_argument("-p", dest="preferred", default="efo,hp,ordo,cttv,eva-clinvar,gwas,atlas")
 
         args = parser.parse_args(args=argv[1:])
 
         self.input_filepath = args.input_filepath
         self.output_filepath = args.output_filepath
+
+        self.filters = {"ontologies": args.ontologies, "required": args.required, "preferred": args.preferred}
 
 
 if __name__ == "__main__":
