d505cca991c24b2114f22eac21512a78813d0a22
Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>
2016-09-29 16:37:48 +0100

    Now querying OLS to retrieve the ontology label, plus some code refactoring to make the code a bit more object-oriented

--- resources/oct16/mapping/traits_to_zooma.py
+++ resources/oct16/mapping/traits_to_zooma.py
@@ -3,79 +3,121 @@ import requests
 import sys
 
 
+class Trait:
+    
+    def __init__(self, name, frequency):
+        self.name = name
+        self.frequency = frequency
+    
+    def __str__(self):
+        return "{}\t{}".format(self.name, self.frequency)
+
+
+class OntologyMapping:
+    
+    def __init__(self, uri, label, confidence, source):
+        self.uri = uri
+        self.label = label
+        self.confidence = confidence
+        self.source = source
+    
+    def __str__(self):
+        return "{}\t{}\t{}\t{}".format(self.label, self.uri, self.confidence, self.source)
+
+
+
 def main():
     parser = ArgParser(sys.argv)
 
-    trait_name_tuples = get_trait_name_tuples_from_file(parser.input_filepath)
-    parse_and_write_output(trait_name_tuples, parser.output_filepath, parser.filters)
+    # Read trait names from input file
+    traits = read_traits(parser.input_filepath)
+    
+    with open(parser.output_filepath, "wt") as output_file:
+        output_file.write("#{}\n".format(parser.filters))
+        for trait in traits:
+            output_file.write(str(trait))
+            for mapping in get_ontology_mappings(trait, parser.filters):
+                output_file.write("\t" + str(mapping))
+            output_file.write("\n")
+            
 
 
-def parse_and_write_output(trait_name_tuples, output_filepath, filters):
-    with open(output_filepath, "wt") as output_file:
-        output_file.write("#{}\n".format(filters))
-        for trait_name_tuple in trait_name_tuples:
-            trait_name = trait_name_tuple[0]
-            zooma_response = query_zooma(trait_name, filters)
-            if zooma_response == 0:
-                continue
-            output_list = zooma_response_to_list(zooma_response, trait_name_tuple)
-
-            output_file.write("\t".join(output_list) + "\n")
-
-
-def get_trait_name_tuples_from_file(filepath):
-    trait_name_tuples = []
+def read_traits(filepath):
+    traits = []
     with open(filepath) as f:
         for line in f:
             line = line.rstrip()
             line_list = line.split("\t")
-            trait_name = line_list[0]
-            freq = line_list[1]
-            trait_name_tuples.append((trait_name, freq))
-    return trait_name_tuples
+            name = line_list[0]
+            frequency = line_list[1]
+            traits.append(Trait(name, frequency))
+    return traits
 
 
-def form_zooma_query(trait_name, filters):
+def write_ontology_mappings(ontology_mappings, filters, output_filepath):
+    with open(output_filepath, "wt") as output_file:
+        output_file.write("#{}\n".format(filters))
+        for mapping in ontology_mappings:
+            output_file.write("\t".join(mapping) + "\n")
+
+
+def get_ontology_mappings(trait, filters):
+    '''
+    First get the URI, label from a selected source, confidence and source:
+    http://snarf.ebi.ac.uk:8580/spot/zooma/v2/api/services/annotate?propertyValue=intellectual+disability
+    Then the ontology label to replace the label from a source:
+    http://www.ebi.ac.uk/ols/api/terms?iri=http%3A%2F%2Fwww.ebi.ac.uk%2Fefo%2FEFO_0003847
+    '''
+    url = build_zooma_query(trait.name, filters)
+    json_response_1 = requests.get(url).json()
+    
+    mappings = get_mappings_for_trait(json_response_1, trait)
+    
+    for mapping in mappings:
+        label = get_ontology_label_from_ols(mapping.uri)
+        # If no label is returned (shouldn't really happen) keep the existing one
+        if label:
+            mapping.label = label
+    
+    return mappings
+
+
+def build_zooma_query(trait_name, filters):
     url_filters = []
     url = "http://snarf.ebi.ac.uk:8580/spot/zooma/v2/api/services/annotate?propertyValue={}".format(trait_name)
     url_filters.append("required:[{}]".format(filters["required"]))
     url_filters.append("ontologies:[{}]".format(filters["ontologies"]))
     url_filters.append("preferred:[{}]".format(filters["preferred"]))
     url += "&filter={}".format(",".join(url_filters))
-
     return url
 
 
-def query_zooma(trait_name, filters):
-    url = form_zooma_query(trait_name, filters)
-    r = requests.get(url)
-    try:
-        json_response = r.json()
-    except Exception as e:
-        # print("\t".join([trait_name, e, r]))
-        return 0
-    return json_response
-
-
-def result_to_tuple(result):
-    label = result["annotatedProperty"]["propertyValue"]
-    uri = ",".join(result["semanticTags"])
-    confidence = result["confidence"]
-    source_name = result["derivedFrom"]["provenance"]["source"]["name"]
-
-    result_tuple = (label, uri, confidence, source_name)
-
-    return result_tuple
-
-
-def zooma_response_to_list(zooma_response, trait_name_tuple):
-    trait_name = trait_name_tuple[0]
-    freq = trait_name_tuple[1]
-    output_list = [trait_name, freq]
+def get_mappings_for_trait(zooma_response, trait):
+    mappings = []
     for result in zooma_response:
-        output_list.extend(result_to_tuple(result))
+        uri = ",".join(result["semanticTags"])
+        label = result["annotatedProperty"]["propertyValue"]
+        confidence = result["confidence"]
+        source_name = result["derivedFrom"]["provenance"]["source"]["name"]
+        mappings.append(OntologyMapping(uri, label, confidence, source_name))
+    return mappings
 
-    return output_list
+
+def get_ontology_label_from_ols(uri_mapping):
+    url = build_ols_query(uri_mapping)
+    json_response = requests.get(url).json()
+    for term in json_response["_embedded"]["terms"]:
+        if term["is_defining_ontology"]:
+            return term["label"]
+    
+    return None
+
+
+def build_ols_query(ontology_uri):
+    url = "http://www.ebi.ac.uk/ols/api/terms?iri={}".format(ontology_uri)
+    #print(url)
+    return url
+
 
 
 class ArgParser:
