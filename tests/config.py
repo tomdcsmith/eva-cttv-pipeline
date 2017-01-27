01ec8712fe8793f51e343727bdb63a827854fe09
Tom Smith <tsmith@ebi.ac.uk>
2017-01-27 13:21:48 +0000

    Renamed snp2gene_extract file for consistency between versions.

--- tests/config.py
+++ tests/config.py
@@ -2,4 +2,4 @@ import os
 
 test_dir = os.path.dirname(__file__)
 
-snp_2_gene_file = os.path.join(test_dir, "resources/coords_20170117_out_extract.tsv")
+snp_2_gene_file = os.path.join(test_dir, "resources/snp2gene_extract.tsv")
