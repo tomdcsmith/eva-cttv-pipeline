2c51e0552ea753fbaf9926ad51342166045e4278
Tom Smith <tsmith@ebi.ac.uk>
2017-07-24 15:35:50 +0100

    - Fixed ranking of distance measure in OXOmapping - Output only current terms in efo

--- bin/trait_mapping/trait_mapping.py
+++ bin/trait_mapping/trait_mapping.py
@@ -65,8 +65,8 @@ class OxOMapping:
                 self.is_current == other.is_current, self.ontology_label == other.ontology_label)
 
     def __lt__(self, other):
-        return ((self.distance, self.in_efo, self.is_current) <
-                (other.distance, other.in_efo, other.is_current))
+        return ((other.distance, self.in_efo, self.is_current) <
+                (self.distance, other.in_efo, other.is_current))
 
 
 class OxOResult:
@@ -245,6 +245,27 @@ def output_trait_mapping(trait: Trait, mapping_writer: csv.writer):
         mapping_writer.writerow([trait.name, ontology_entry.uri, ontology_entry.label])
 
 
+def get_zooma_mappings_for_curation(trait: Trait) -> list:
+    zooma_entry_list = []
+    for zooma_mapping in trait.zooma_mapping_list:
+        for zooma_entry in zooma_mapping.zooma_entry_list:
+            if zooma_entry.in_efo and zooma_entry.is_current:
+                zooma_entry_list.append(zooma_entry)
+    zooma_entry_list.sort(reverse=True)
+    return zooma_entry_list
+
+
+# TODO best way to unite this and the above very similar method?
+def get_oxo_mappings_for_curation(trait: Trait) -> list:
+    oxo_mapping_list = []
+    for oxo_result in trait.oxo_xref_list:
+        for oxo_mapping in oxo_result.oxo_mapping_list:
+            if oxo_mapping.in_efo and oxo_mapping.is_current:
+                oxo_mapping_list.append(oxo_mapping)
+    oxo_mapping_list.sort(reverse=True)
+    return oxo_mapping_list
+
+
 def output_for_curation(trait: Trait, curation_writer: csv.writer):
     """
     Write any non-finished Zooma or OxO mappings of a trait to a file for manual curation.
@@ -256,16 +277,14 @@ def output_for_curation(trait: Trait, curation_writer: csv.writer):
     output_row = []
     output_row.extend([trait.name, trait.frequency])
 
-    zooma_entry_list = [zooma_entry for zooma_mapping in trait.zooma_mapping_list for zooma_entry in zooma_mapping.zooma_entry_list]
-    zooma_entry_list.sort(reverse=True)
+    zooma_entry_list = get_zooma_mappings_for_curation(trait)
 
     for zooma_entry in zooma_entry_list:
         cell = [zooma_entry.uri, zooma_entry.ontology_label, str(zooma_entry.in_efo),
                 str(zooma_entry.is_current), str(zooma_entry.confidence), zooma_entry.source]
         output_row.append("|".join(cell))
 
-    oxo_mapping_list = [oxo_mapping for oxo_result in trait.oxo_xref_list for oxo_mapping in oxo_result.oxo_mapping_list]
-    oxo_mapping_list.sort(reverse=True)
+    oxo_mapping_list = get_oxo_mappings_for_curation(trait)
 
     for oxo_mapping in oxo_mapping_list:
         cell = [str(oxo_mapping.uri), oxo_mapping.ontology_label, str(oxo_mapping.in_efo),
