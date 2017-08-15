e2b3631d84d6983f7cf21424fbe03e4c8bb76b85
Tom Smith <tsmith@ebi.ac.uk>
2017-08-15 09:49:58 +0100

    get_mappings_for_curation to process both zooma and oxo results for output

--- eva_cttv_pipeline/trait_mapping/output.py
+++ eva_cttv_pipeline/trait_mapping/output.py
@@ -14,27 +14,15 @@ def output_trait_mapping(trait: Trait, mapping_writer: csv.writer):
         mapping_writer.writerow([trait.name, ontology_entry.uri, ontology_entry.label])
 
 
-def get_zooma_mappings_for_curation(trait: Trait) -> list:
-    """Sorted in reverse so the highest ranked zooma entries are shown first"""
-    mapping_list = []
-    for zooma_result in trait.zooma_result_list:
-        for zooma_mapping in zooma_result.mapping_list:
-            if zooma_mapping.in_efo and zooma_mapping.is_current:
-                mapping_list.append(zooma_mapping)
-    mapping_list.sort(reverse=True)
-    return mapping_list
-
-
-# TODO best way to unite this and the above very similar method?
-def get_oxo_mappings_for_curation(trait: Trait) -> list:
+def get_mappings_for_curation(result_list) -> list:
     """Sorted in reverse so the highest ranked oxo mappings are shown first"""
-    oxo_mapping_list = []
-    for oxo_result in trait.oxo_result_list:
-        for oxo_mapping in oxo_result.mapping_list:
-            if oxo_mapping.in_efo and oxo_mapping.is_current:
-                oxo_mapping_list.append(oxo_mapping)
-    oxo_mapping_list.sort(reverse=True)
-    return oxo_mapping_list
+    curation_mapping_list = []
+    for result in result_list:
+        for mapping in result.mapping_list:
+            if mapping.in_efo and mapping.is_current:
+                curation_mapping_list.append(mapping)
+    curation_mapping_list.sort(reverse=True)
+    return curation_mapping_list
 
 
 def output_for_curation(trait: Trait, curation_writer: csv.writer):
@@ -47,14 +35,14 @@ def output_for_curation(trait: Trait, curation_writer: csv.writer):
     """
     output_row = [trait.name, trait.frequency]
 
-    zooma_mapping_list = get_zooma_mappings_for_curation(trait)
+    zooma_mapping_list = get_mappings_for_curation(trait.zooma_result_list)
 
     for zooma_mapping in zooma_mapping_list:
         cell = [zooma_mapping.uri, zooma_mapping.ontology_label, str(zooma_mapping.confidence),
                 zooma_mapping.source]
         output_row.append("|".join(cell))
 
-    oxo_mapping_list = get_oxo_mappings_for_curation(trait)
+    oxo_mapping_list = get_mappings_for_curation(trait.oxo_result_list)
 
     for oxo_mapping in oxo_mapping_list:
         cell = [str(oxo_mapping.uri), oxo_mapping.ontology_label, str(oxo_mapping.distance),
