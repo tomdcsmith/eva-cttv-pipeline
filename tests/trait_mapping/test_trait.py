ccbe574562f6903f6c85ce05afcc550d95b16b88
Tom Smith <tsmith@ebi.ac.uk>
2017-08-16 09:54:31 +0100

    Test for process_oxo_mappings

--- tests/trait_mapping/test_trait.py
+++ tests/trait_mapping/test_trait.py
@@ -2,6 +2,7 @@ import unittest
 
 import eva_cttv_pipeline.trait_mapping.trait as trait
 import eva_cttv_pipeline.trait_mapping.zooma as zooma
+import eva_cttv_pipeline.trait_mapping.oxo as oxo
 
 
 class TestTrait(unittest.TestCase):
@@ -32,6 +33,28 @@ class TestTrait(unittest.TestCase):
         test_trait.zooma_result_list.append(test_zooma_result)
 
         test_trait.process_zooma_results()
+        self.assertEqual(1, len(test_trait.finished_mapping_set))
+
+    def test_process_oxo_mappings(self):
+        test_trait = trait.Trait('congenital cystic disease of liver', 11)
+
+        test_oxo_result = oxo.OxOResult('HP:0006706', 'Cystic liver disease', 'HP:0006706')
+
+        test_oxo_mapping_1 = oxo.OxOMapping('Isolated polycystic liver disease', 'Orphanet:2924', 2,
+                                            'HP:0006706')
+        test_oxo_mapping_1.in_efo = True
+        test_oxo_mapping_1.is_current = True
+
+        test_oxo_mapping_2 = oxo.OxOMapping('cystic liver disease', 'EFO:1001505', 1, 'HP:0006706')
+        test_oxo_mapping_2.in_efo = True
+        test_oxo_mapping_2.is_current = True
+
+        test_oxo_result.mapping_list = [test_oxo_mapping_1, test_oxo_mapping_2]
+
+        test_trait.oxo_result_list.append(test_oxo_result)
+
+        test_trait.process_oxo_mappings()
+
         self.assertEqual(1, len(test_trait.finished_mapping_set))
 
 
